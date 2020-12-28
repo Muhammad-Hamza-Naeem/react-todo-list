936ff4384854cacaa0bbf3cf303557ee5f26a508
MUHAMMAD HAMZA NAEEM <m.hamza_naeem@outlook.com>
2020-12-28 18:37:44 +0500

    Submit and change

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import {v1 as uuid} from "uuid";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoInput from './Components/TodoInput'
 import TodoList from './Components/TodoList'
-//fghjklfghj
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 class App extends Component {
   state = {
-    items: [{ id: 1, title: 'wake up' }, { id: 2, title: 'make breakfast' }],
+    items: [],
     id: uuid(),
     item: '',
     editItem:false
   };
   handleChange = (e) => {
-
+    this.setState({
+      item: e.target.value
+    })
   }
   handleSubmit = (e) => {
-
-  }
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    }
+    const updatedItems = [...this.state.items, newItem]
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuid(),
+      editItem:false
+      
+    },()=>console.log(this.state))
+  };
   clearList = () => {
 
   }
@@ -26,14 +40,13 @@ class App extends Component {
   }
   handleDelete = (id) => {}
   render() {
-
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md8 mt-5">
             <h3 className="text-capitalize text-center"></h3>
             <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.state.editItem}/>
-            <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit}/>
+            <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit}/> 
           </div>
         </div>
       </div>
